19216cf01cae94ab81cc4a93c4c9e5351a7780ad
Biggie009 <85871114+Biggie009@users.noreply.github.com>
2021-06-16 17:09:42 +0530

    Update disasters.py

--- LightYagami/modules/disasters.py
+++ LightYagami/modules/disasters.py
@@ -3,7 +3,7 @@
 	import os
 	from typing import Optional
 	
-	from SaitamaRobot import (
+	from LightYagami import (
 	    DEV_USERS,
 	    OWNER_ID,
 	    DRAGONS,
@@ -13,18 +13,19 @@
 	    WOLVES,
 	    dispatcher,
 	)
-	from SaitamaRobot.modules.helper_funcs.chat_status import (
+	from LightYagami.modules.helper_funcs.chat_status import (
 	    dev_plus,
 	    sudo_plus,
 	    whitelist_plus,
 	)
-	from SaitamaRobot.modules.helper_funcs.extraction import extract_user
-	from SaitamaRobot.modules.log_channel import gloggable
+	from LightYagami.modules.helper_funcs.extraction import extract_user
+	from LightYagami.modules.log_channel import gloggable
 	from telegram import ParseMode, TelegramError, Update
 	from telegram.ext import CallbackContext, CommandHandler, run_async
 	from telegram.utils.helpers import mention_html
 	
-	ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "SaitamaRobot/elevated_users.json")
+	ELEVATED_USERS_FILE = os.path.join(os.getcwd(),
+                                   'LightYagami/elevated_users.json')
 	
 	
 	def check_user_id(user_id: int, context: CallbackContext) -> Optional[str]:
