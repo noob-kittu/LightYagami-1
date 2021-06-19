d62c1ee73c213b3573691888b6600dfbf62e0b73
Biggie009 <85871114+Biggie009@users.noreply.github.com>
2021-06-19 18:53:56 +0530

    Update __init__.py

--- LightYagami/__init__.py
+++ LightYagami/__init__.py
@@ -8,7 +8,6 @@ from pyrogram import Client, errors
 from aiogram import Bot, Dispatcher, types
 from aiogram.bot.api import TELEGRAM_PRODUCTION, TelegramAPIServer
 from telethon import TelegramClient
-from motor import motor_asyncio
 from odmantic import AIOEngine
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
